2e085a6a6c043f91e2473e73f2c4a545adb37b2f
Ogagaphizzy <ogagaphizzy@gmail.com>
2026-09-07 14:16:46 +0100

    added new files

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const hamburgerBtnElement = document.getElementById('hamburger')
+const closeBtnElement = document.getElementById('close')
+const modalElement = document.getElementById('modal')
+const registerHtmlElement = document.getElementById('register')
+const mainBody = document.getElementById('main')
+const headerBody = document.getElementById('header')
+const footerBody = document.getElementById('footer')
+const smartHome = document.getElementById('smartHomes')
+const mainHeader = document.getElementById('main-header')
+const bodyElement = document.getElementById('page')
+const faqCards = document.querySelectorAll('.FAQs-info')
+
+hamburgerBtnElement.addEventListener('click', openOverlay)
+closeBtnElement.addEventListener('click', closeOverlay)
+
+for (const faqCard of faqCards) {
+    const crossImgElement = faqCard.querySelector('.cross')
+    const minusImgElement = faqCard.querySelector('.minus')
+    const faqAnswer = faqCard.querySelector('.answer')
+
+    crossImgElement.addEventListener('click', function () {
+        for (const otherFaqCard of faqCards) {
+            if (otherFaqCard !== faqCard) {
+                otherFaqCard.querySelector('.answer').style.display = 'none'
+                otherFaqCard.querySelector('.cross').style.display = 'block'
+                otherFaqCard.querySelector('.minus').style.display = 'none'
+            }
+        }
+
+        faqAnswer.style.display = 'block'
+        crossImgElement.style.display = 'none'
+        minusImgElement.style.display = 'block'
+    })
+
+    minusImgElement.addEventListener('click', function () {
+        faqAnswer.style.display = 'none'
+        crossImgElement.style.display = 'block'
+        minusImgElement.style.display = 'none'
+    })
+}
---
+++ web.js
@@ -0,0 +1,66 @@
+let navigation = performance.getEntriesByType("navigation") [0];
+if (navigation && navigation.type === "reload") {
+    history.replaceState(
+        null, 
+        "", 
+        window.location.pathname + window.location.search
+    );
+}
+
+function openOverlay() {
+    hamburgerBtnElement.style.display = 'none'
+    closeBtnElement.style.display = 'block'
+    modalElement.style.display = 'flex'
+    registerHtmlElement.style.display = 'block'
+    modalElement.style.backgroundColor = '#2559f6'
+    registerHtmlElement.style.backgroundColor = '#2559f6'
+    modalElement.style.height = '100vh'
+    registerHtmlElement.style.height = '100vh'
+    mainHeader.style.display = 'none'
+    mainBody.style.display = 'none'
+    footerBody.style.display = 'none'
+    window.location.hash = "page"
+    headerBody.style.backgroundColor = "#2559f6"
+}
+// CLOSE
+function closeOverlay() {
+    hamburgerBtnElement.style.display = 'block'
+    closeBtnElement.style.display = 'none'
+    modalElement.style.display = 'none'
+    registerHtmlElement.style.display = 'none'
+    smartHome.style.display = 'block'
+    mainHeader.style.display = 'block'
+    mainBody.style.display = 'block'
+    headerBody.style.display = 'block'
+    footerBody.style.display = 'block'
+}
+
+window.addEventListener("resize", function () {
+    const isDesktop = window.innerWidth >= 900
+    if (isDesktop) {
+        hamburgerBtnElement.style.display = 'none'
+        closeBtnElement.style.display = 'none'
+        modalElement.style.display = 'flex'
+        registerHtmlElement.style.display = 'flex'
+        mainHeader.style.display = 'block'
+        mainBody.style.display = 'block'
+        footerBody.style.display = 'block'
+    } else {
+        hamburgerBtnElement.style.display = 'block'
+        closeBtnElement.style.display = 'none'
+        modalElement.style.display = 'none'
+        registerHtmlElement.style.display = 'none'
+        smartHome.style.display = 'block'
+        mainHeader.style.display = 'block'
+        mainBody.style.display = 'block'
+        footerBody.style.display = 'block'
+    }
+
+    wasDesktop = isDesktop;
+})
+
+// window.addEventListener("load", function () {
+//     if (window.loacation.hash === '#page') {
+//         history.replaceState(null, "", window.location.pathname + window.location.search)
+//     }
+// })
